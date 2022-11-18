f56ea9eb0192f334e6d58659df614a9985c97e67
Hippolyte Salmon <hipposalmon@gmail.com>
2022-11-18 08:34:37 +0100

    Ouais la fonction fonctionne (lol)

--- TP1 Python.py
+++ TP1 Python.py
@@ -1,10 +1,7 @@
 def is_bissextile(annee):
-    bissextile = False
-    if annee%400 == 0 or (annee%4 == 0 and annee%100 != 0):
-        print("L'année est bissextile")
-    else:
-        print("L'année n'est pas bissextile")
 
-annee = int(input("Veuillez entrer l'année"))
+    print(annee%400 == 0 or (annee%4 == 0 and annee%100 != 0))
 
-is_bissextile(annee)
+annee_s = int(input("Veuillez entrer l'année"))
+
+is_bissextile(annee_s)
