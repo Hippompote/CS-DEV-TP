9145e0a3db49ebf813621ccc6c49050bcaca7469
Hippolyte Salmon <hipposalmon@gmail.com>
2022-11-18 09:16:42 +0100

    Fonctions dates

--- TP1 Python.py
+++ TP1 Python.py
@@ -2,6 +2,28 @@ def is_bissextile(annee):
 
     print(annee%400 == 0 or (annee%4 == 0 and annee%100 != 0))
 
-annee_s = int(input("Veuillez entrer l'année"))
 
+def nb_jours(mois,annee):
+
+    if mois == 2 and is_bissextile(annee):
+        return 29
+    elif mois == 2:
+        return 28
+    elif mois in [4,6,9,11]:
+        return 30
+    else:
+        return 31
+
+def is_date_valid(jour,mois,annee):
+    return (jour > 0 and jour < nb_jours(mois,annee) and is_mois_valid(mois,annee))
+
+def is_mois_valid(mois,annee):
+    return ( 0 < mois <=12 )
+
+
+jour_s = int(input("Veuillez entrer un jour"))
+mois_s = int(input("Veuillez entrer un mois"))
+annee_s = int(input("Veuillez entrer l'annee"))
 is_bissextile(annee_s)
+print(nb_jours(mois_s,annee_s))
+print(is_date_valid(jour_s,mois_s,annee_s))
